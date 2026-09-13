82991943babf50190c72b4dad999290e91f4c61f
Amalto2013 <amielmalto2013@gmail.com>
2026-09-13 01:31:54 +0000

    practice

--- lesson1/classwork/practice.py
+++ lesson1/classwork/practice.py
@@ -1,24 +1,32 @@
 # Problem 1
 # Create a variable for your favorite number and print it.
 
-
-
+x=213
+print(x)
 # Problem 2
 # Create a variable for your height in meters and print it.
 
+h=1.65
 
-
+print(h)
 # Problem 3
 # Create a variable for your favorite animal and print it in a full sentence.
 
-
+F_animal= "dog"
+print("My favorite animal is",F_animal )
 
 # Problem 4
 # Create two variables: one for your first name and one for your last name.
 # Print them on 2 separate lines.
-
+Fn="Amiel"
+Ln="Malto"
+print(Fn)
+print(Ln)
 
 
 # Problem 5
 # Create 3 variables: your first name, your age, and your favorite color.
 # Print them on the same line in a full sentence.
+Age=13
+Fc="red"
+print(Fn,Age,Fc)
